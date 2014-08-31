5ea212dc19f67f44d0c599bc70e163eb018489ce
Roman Matěna <info@romanmatena.cz>
2014-08-31 13:44:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Roadtrippers fixed printing</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<p>open the itinerary page in roadtripper, copy the page source, in firefox CTRL+U, select all and insert into this form</p>
+		<form method="POST">
+			<
+		</form>
+	</body>
+</html>
